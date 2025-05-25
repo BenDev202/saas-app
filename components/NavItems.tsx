2e0a252b41ce6bb2dcc4dd38a3146ecb5cf2ed68
armand benjamin <armandbenjamin30@gmail.com>
2025-05-25 18:03:59 +0200

    style: update NavItems component and add link-primary class for active links

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,26 +1,31 @@
-'use client'
-import { cn } from "@/lib/utils"
-import Link from "next/link"
-import { usePathname } from "next/navigation"
+"use client";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const navItems = [
-  { label: 'Home', href: '/' },
-  { label: 'Companions', href: '/companions' },
-  { label: 'My Journey', href: '/my-journey' },
-]
+  { label: "Home", href: "/" },
+  { label: "Companions", href: "/companions" },
+  { label: "My Journey", href: "/my-journey" },
+];
 
 const NavItems = () => {
-
-    const  pathname = usePathname();
+  const pathname = usePathname();
   return (
     <nav className="flex items-center gap-4">
-        {navItems.map(({label, href }) => (
-            <Link href={href} key={label} className={cn(pathname === href && 'textprimary font-semibold')}>
-                {label}
-            </Link>
-        ))}
+      {navItems.map(({ label, href }) => (
+        <Link
+          href={href}
+          key={label}
+          className={cn(
+            pathname === href && "link-primary font-semibold"
+          )}
+        >
+          {label}
+        </Link>
+      ))}
     </nav>
-  )
-}
+  );
+};
 
-export default NavItems
+export default NavItems;
