ad3cd4adf08d0661f1c469ee4db5ab38195336d3
armand benjamin <armandbenjamin30@gmail.com>
2025-05-26 18:09:49 +0200

    feat: add SignUp component to sign-up page

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+export default function page() {
+  return (
+    <main className="flex items-center justify-center">
+      <SignUp />
+    </main>
+    
+  )
+}
