eaed6f1b304c61908d2c0d8842e3e9f17edf5e9a
armand benjamin <armandbenjamin30@gmail.com>
2025-05-26 16:28:07 +0200

    feat: implement form structure and submission handling in CompanionForm

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -52,7 +52,27 @@ const CompanionForm = () => {
 
 
   return (
-    <div>CompanionForm</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
 
