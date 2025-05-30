e0fc057dd6902787a3fb29593227c52bb12ce9f2
armand benjamin <armandbenjamin30@gmail.com>
2025-05-30 11:01:55 +0200

    change of the codes

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,11 +1,7 @@
 import { SignIn } from '@clerk/nextjs'
-import React from 'react'
 
-export default function page() {
-  return (
-    <main className="flex items-center justify-center">
-      <SignIn />
+export default function Page() {
+    return <main className="flex items-center justify-center">
+        <SignIn />
     </main>
-    
-  )
 }
